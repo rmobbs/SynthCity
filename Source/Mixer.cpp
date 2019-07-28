3a65bd03f1801e5d6328c81aba00477d6511cbdf
Robert Mobbs <r_mobbs@hotmail.com>
2019-07-28 00:41:30 -0500

    Faster sound/process expiry

--- Source/Mixer.cpp
+++ Source/Mixer.cpp
@@ -38,11 +38,11 @@ private:
   static int32 nextVoiceId;
 public:
   // Instances
-  std::array<SoundInstance*, Patch::kMaxSounds * 2> sounds = { nullptr };
-  uint32 curSounds = 0;
+  std::array<SoundInstance*, Patch::kMaxSounds> sounds = { nullptr };
+  uint32 bitSounds = 0;
   uint32 numSounds = 0;
-  std::array<ProcessInstance*, Patch::kMaxProcesses * 2> processes = { nullptr };
-  uint32 curProcesses = 0;
+  std::array<ProcessInstance*, Patch::kMaxProcesses> processes = { nullptr };
+  uint32 bitProcesses = 0;
   uint32 numProcesses = 0;
 
   // Frame counter
@@ -58,17 +58,21 @@ public:
 
   Voice(const Patch* patch, float volume)
   : volume(volume) {
-    for (uint32 s = 0; s < patch->sounds.size(); ++ s) {
+    bitSounds = 0;
+    for (uint32 s = 0; s < patch->sounds.size(); ++s) {
+      bitSounds |= 1 << s;
       sounds[s] = SoundInstanceFreeList::FreeList(patch->
         sounds[s]->GetClassHash()).Borrow(patch->sounds[s]);
     }
-    curSounds = numSounds = patch->sounds.size();
+    numSounds = patch->sounds.size();
 
-    for (uint32 p = 0; p < patch->processes.size(); ++ p) {
+    bitProcesses = 0;
+    for (uint32 p = 0; p < patch->processes.size(); ++p) {
+      bitProcesses |= 1 << p;
       processes[p] = ProcessInstanceFreeList::FreeList(patch->processes[p]->
         GetClassHash()).Borrow(patch->processes[p], patch->GetSoundDuration());
     }
-    curProcesses = numProcesses = patch->processes.size();
+    numProcesses = patch->processes.size();
 
     voiceId = nextVoiceId++;
   }
@@ -183,81 +187,67 @@ bool Mixer::Init(uint32 audioBufferSize) {
 }
 
 void Mixer::MixVoices(float* mixBuffer, uint32 numFrames) {
-  // Clear the buffer
-  memset(mixBuffer, 0, numFrames * sizeof(float) * 2);
+  uint32 maxFrames = 0;
+
+  // Fill remainder with zeroes
+  std::memset(mixBuffer, 0, numFrames * sizeof(float) * 2);
 
   if (voices.size() > 0) {
-
-    static constexpr float kPeakVolumeRatio = 0.7f;
-    static constexpr float kVolumeEpsilon = 0.01f;
-
     // Mix active voices
     auto voiceIter = voices.begin();
     while (voiceIter != voices.end()) {
-      for (uint32 f = 0; f < numFrames; ++f) {
+      auto v = *voiceIter;
+
+      uint32 outFrame = 0;
+      while (outFrame < numFrames) {
         float samples[2] = { 0 }; // Stereo
 
-        auto v = *voiceIter;
-        if (v->curSounds > 0) {
-          for (uint32 i = 0; i < v->numSounds; ++i) {
-            auto s = v->sounds[i];
-            if (s != nullptr) {
-              if (s->GetSamplesForFrame(samples, 2, v->frame) != 2) {
-                std::swap(v->sounds[i], v->sounds[i + v->numSounds]);
-                --v->curSounds;
-              }
+        for (uint32 i = 0; i < v->numSounds; ++i) {
+          if (v->bitSounds & (1 << i)) {
+            if (v->sounds[i]->GetSamplesForFrame(samples, 2, v->frame) != 2) {
+              v->bitSounds &= ~(1 << i);
             }
           }
         }
 
-        if (v->curProcesses > 0) {
-          for (uint32 i = 0; i < v->numProcesses; ++i) {
-            auto p = v->processes[i];
-            if (p != nullptr) {
-              if (!p->ProcessSamples(samples, 2, v->frame)) {
-                std::swap(v->processes[i], v->processes[i + v->numProcesses]);
-                --v->curProcesses;
-
-                if (p->process->StopPatchOnEnd()) {
-                  for (uint32 j = 0; j < v->numProcesses; ++j) {
-                    if (v->processes[j]) {
-                      std::swap(v->processes[j], v->processes[j + v->numProcesses]);
-                    }
-                  }
-                  v->curProcesses = 0;
-                }
-              }
+        for (uint32 i = 0; i < v->numProcesses; ++i) {
+          if (v->bitProcesses & (1 << i)) {
+            if (!v->processes[i]->ProcessSamples(samples, 2, v->frame)) {
+              v->bitProcesses &= ~(1 << i);
             }
           }
         }
 
-        if (v->curSounds > 0 && (v->curProcesses > 0 || !v->numProcesses)) {
-          mixBuffer[f * 2 + 0] += samples[0] * masterVolume * v->volume * kPeakVolumeRatio;
-          mixBuffer[f * 2 + 1] += samples[1] * masterVolume * v->volume * kPeakVolumeRatio;
-
-          ++v->frame;
-        }
-        else {
+        if (v->bitSounds == 0 || (v->numProcesses > 0 && v->bitProcesses == 0)) {
           break;
         }
+
+        mixBuffer[outFrame * 2 + 0] += samples[0] * masterVolume * v->volume * kPeakVolumeRatio;
+        mixBuffer[outFrame * 2 + 1] += samples[1] * masterVolume * v->volume * kPeakVolumeRatio;
+
+        ++v->frame;
+        ++outFrame;
       }
 
-      if ((*voiceIter)->curSounds > 0 && ((*voiceIter)->curProcesses > 0 || !(*voiceIter)->numProcesses)) {
-        ++voiceIter;
+      if (maxFrames < outFrame) {
+        maxFrames = outFrame;
       }
-      else {
+
+      if (v->bitSounds == 0 || (v->numProcesses > 0 && v->bitProcesses == 0)) {
         expiredVoices.push(*voiceIter);
-
         voiceMap.erase((*voiceIter)->voiceId);
         voiceIter = voices.erase(voiceIter);
+      }
+      else {
+        ++voiceIter;
       }
     }
 
     // Clip so we don't distort
-    for (uint32 f = 0; f < numFrames; ++f) {
-      mixBuffer[f * 2 + 0] = std::max(std::min(mixBuffer[f * 2 + 0], 0.7f), -0.7f);
-      mixBuffer[f * 2 + 1] = std::max(std::min(mixBuffer[f * 2 + 1], 0.7f), -0.7f);
-    };
+    for (uint32 curFrame = 0; curFrame < maxFrames; ++curFrame) {
+      mixBuffer[curFrame * 2 + 0] = std::max(std::min(mixBuffer[curFrame * 2 + 0], 0.7f), -0.7f);
+      mixBuffer[curFrame * 2 + 1] = std::max(std::min(mixBuffer[curFrame * 2 + 1], 0.7f), -0.7f);
+    }
 
     // So people can query this without locking
     numActiveVoices = voices.size();
@@ -267,8 +257,7 @@ void Mixer::MixVoices(float* mixBuffer, uint32 numFrames) {
 void Mixer::WriteOutput(float *input, int16 *output, int32 frames) {
   int32 i = 0;
   frames *= 2;	// Stereo
-  while (i < frames)
-  {
+  while (i < frames) {
     output[i] = static_cast<int16>(input[i] * SHRT_MAX);
     ++i;
     output[i] = static_cast<int16>(input[i] * SHRT_MAX);
@@ -339,17 +328,13 @@ void Mixer::DrainExpiredPool() {
   while (!expiredVoices.empty()) {
     auto v = expiredVoices.front();
     expiredVoices.pop();
-    for (uint32 si = 0; si < v->sounds.size(); ++si) {
-      if (v->sounds[si] != nullptr) {
-        SoundInstanceFreeList::FreeList(v->sounds[si]->GetSoundHash()).Return(v->sounds[si]);
-        v->sounds[si] = nullptr;
-      }
+    for (uint32 si = 0; si < v->numSounds; ++si) {
+      assert(v->sounds[si] != nullptr);
+      SoundInstanceFreeList::FreeList(v->sounds[si]->GetSoundHash()).Return(v->sounds[si]);
     }
-    for (uint32 pi = 0; pi < v->processes.size(); ++pi) {
-      if (v->processes[pi] != nullptr) {
-        ProcessInstanceFreeList::FreeList(v->processes[pi]->GetProcessHash()).Return(v->processes[pi]);
-        v->processes[pi] = nullptr;
-      }
+    for (uint32 pi = 0; pi < v->numProcesses; ++pi) {
+      assert(v->processes[pi] != nullptr);
+      ProcessInstanceFreeList::FreeList(v->processes[pi]->GetProcessHash()).Return(v->processes[pi]);
     }
     voiceFreeList.Return(v);
   }
